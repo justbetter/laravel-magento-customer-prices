96244520122a899f3cdb8ae1ce9401f77f8f91ad
Ramon Rietdijk <ramon@justbetter.nl>
2025-07-25 11:27:29 +0200

    Defer retrieving all

--- src/Actions/Retrieval/RetrieveAllCustomerPrices.php
+++ src/Actions/Retrieval/RetrieveAllCustomerPrices.php
@@ -4,17 +4,48 @@ namespace JustBetter\MagentoCustomerPrices\Actions\Retrieval;
 
 use Illuminate\Foundation\Bus\PendingDispatch;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Enumerable;
 use JustBetter\MagentoCustomerPrices\Contracts\Retrieval\RetrievesAllCustomerPrices;
 use JustBetter\MagentoCustomerPrices\Jobs\Retrieval\RetrieveCustomerPriceJob;
+use JustBetter\MagentoCustomerPrices\Models\CustomerPrice;
 use JustBetter\MagentoCustomerPrices\Repository\BaseRepository;
 
 class RetrieveAllCustomerPrices implements RetrievesAllCustomerPrices
 {
-    public function retrieve(?Carbon $from): void
+    public function retrieve(?Carbon $from = null, bool $defer = true): void
     {
         $repository = BaseRepository::resolve();
 
-        $repository->skus($from)->each(fn (string $sku): PendingDispatch => RetrieveCustomerPriceJob::dispatch($sku));
+        if (! $defer) {
+            $repository->skus($from)->each(fn (string $sku): PendingDispatch => RetrieveCustomerPriceJob::dispatch($sku));
+
+            return;
+        }
+
+        $date = now();
+
+        $repository->skus($from)->chunk(250)->each(function (Enumerable $skus) use ($date): void {
+            $existing = CustomerPrice::query()
+                ->whereIn('sku', $skus)
+                ->pluck('sku');
+
+            CustomerPrice::query()
+                ->whereIn('sku', $existing)
+                ->where('sync', '=', true)
+                ->update(['retrieve' => true]);
+
+            CustomerPrice::query()->insert(
+                $skus
+                    ->diff($existing)
+                    ->values()
+                    ->map(fn (string $sku): array => [
+                        'sku' => $sku,
+                        'retrieve' => true,
+                        'created_at' => $date,
+                        'updated_at' => $date,
+                    ])->toArray()
+            );
+        });
     }
 
     public static function bind(): void
--- tests/Actions/Retrieval/RetrieveAllCustomerPricesTest.php
+++ tests/Actions/Retrieval/RetrieveAllCustomerPricesTest.php
@@ -5,6 +5,7 @@ namespace JustBetter\MagentoCustomerPrices\Tests\Actions\Retrieval;
 use Illuminate\Support\Facades\Bus;
 use JustBetter\MagentoCustomerPrices\Actions\Retrieval\RetrieveAllCustomerPrices;
 use JustBetter\MagentoCustomerPrices\Jobs\Retrieval\RetrieveCustomerPriceJob;
+use JustBetter\MagentoCustomerPrices\Models\CustomerPrice;
 use JustBetter\MagentoCustomerPrices\Tests\Fakes\FakeRepository;
 use JustBetter\MagentoCustomerPrices\Tests\TestCase;
 use JustBetter\MagentoProducts\Models\MagentoProduct;
@@ -16,14 +17,45 @@ class RetrieveAllCustomerPricesTest extends TestCase
     public function it_dispatches_jobs(): void
     {
         config()->set('magento-customer-prices.repository', FakeRepository::class);
+
         Bus::fake();
 
         MagentoProduct::query()->create(['sku' => '::sku::', 'exists_in_magento' => true]);
 
         /** @var RetrieveAllCustomerPrices $action */
         $action = app(RetrieveAllCustomerPrices::class);
-        $action->retrieve(null);
+        $action->retrieve(null, false);
 
         Bus::assertDispatched(RetrieveCustomerPriceJob::class);
+    }
+
+    #[Test]
+    public function it_defers_retrievals(): void
+    {
+        config()->set('magento-prices.repository', FakeRepository::class);
+
+        Bus::fake();
+
+        MagentoProduct::query()->create(['sku' => '::sku-1::', 'exists_in_magento' => true]);
+        MagentoProduct::query()->create(['sku' => '::sku-2::', 'exists_in_magento' => true]);
+        MagentoProduct::query()->create(['sku' => '::sku-3::', 'exists_in_magento' => true]);
+
+        CustomerPrice::query()->create(['sku' => '::sku-1::', 'retrieve' => false]);
+
+        /** @var RetrieveAllCustomerPrices $action */
+        $action = app(RetrieveAllCustomerPrices::class);
+        $action->retrieve(null, true);
+
+        Bus::assertNotDispatched(RetrieveCustomerPriceJob::class);
+
+        $prices = CustomerPrice::query()
+            ->where('retrieve', '=', true)
+            ->pluck('sku');
+
+        $this->assertEquals([
+            '::sku-1::',
+            '::sku-2::',
+            '::sku-3::',
+        ], $prices->toArray());
     }
 }
